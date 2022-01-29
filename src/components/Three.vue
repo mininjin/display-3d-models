9eaec60befd2e6245b758b89e3d7412da8634890
Yuya <Yuya@MacBook-Air.local>
2022-01-29 19:10:07 +0900

    complete throw-balls

--- src/components/Three.vue
+++ src/components/Three.vue
@@ -1,5 +1,6 @@
 <template>
-  <div ref="container" class="fixed w-full h-full top-0 left-0">
+  <div class="fixed w-full h-full top-0 left-0">
+    <div ref="container" class="w-full h-full"></div>
     <div
       class="
         absolute
@@ -24,15 +25,37 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import {
+  AmbientLight,
+  AnimationMixer,
   Color,
   GridHelper,
+  VectorKeyframeTrack,
+  Mesh,
   PerspectiveCamera,
   PointLight,
   Scene,
+  SphereGeometry,
+  Vector3,
   WebGLRenderer,
+  AnimationClip,
+  MeshLambertMaterial,
+  Clock,
+  LoopOnce,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import loader from "@/lib/FileLoader";
+import {
+  GRID_SIZE,
+  BG_COLOR,
+  LIGHT_POSITION,
+  LIGHT_COLOR,
+  INITIAL_CAMERA_POSITION,
+  INITIAL_CAMERA_LOOK,
+  AMBIENT_LIGHT_COLOR,
+  SPHERE_RADIUS,
+  SPHERE_SEGMENTS,
+  ANIMATION_DURATION,
+} from "@/lib/constants";
 
 export default defineComponent({
   setup() {
@@ -43,6 +66,7 @@ export default defineComponent({
     const camera = new PerspectiveCamera();
     const renderer = new WebGLRenderer();
     const light = new PointLight();
+    const ambientLight = new AmbientLight(AMBIENT_LIGHT_COLOR);
     const controls = new OrbitControls(camera, renderer.domElement);
     // 初期化
     const init = () => {
@@ -50,30 +74,46 @@ export default defineComponent({
         // DOMのサイズを取得
         const { clientWidth, clientHeight } = container.value;
         // 背景のグリッドの追加
-        scene.add(new GridHelper(50));
-        scene.background = new Color(0xcccccc);
+        scene.add(new GridHelper(GRID_SIZE));
+        scene.background = new Color(BG_COLOR);
         // ライトの設定
-        light.color.setHex(0xffffff);
-        light.position.set(10, 10, 0);
+        light.color.setHex(LIGHT_COLOR);
+        light.position.set(
+          LIGHT_POSITION.x,
+          LIGHT_POSITION.y,
+          LIGHT_POSITION.z
+        );
         scene.add(light);
+        scene.add(ambientLight);
         // カメラの設定
         camera.aspect = clientWidth / clientHeight;
         camera.updateProjectionMatrix();
-        camera.position.set(10, 10, 0);
-        camera.lookAt(0, 0, 0);
+        camera.position.set(
+          INITIAL_CAMERA_POSITION.x,
+          INITIAL_CAMERA_POSITION.y,
+          INITIAL_CAMERA_POSITION.z
+        );
+        camera.lookAt(INITIAL_CAMERA_LOOK);
         // rendererの設定
         renderer.setSize(clientWidth, clientHeight);
         renderer.setPixelRatio(clientWidth / clientHeight);
         container.value.appendChild(renderer.domElement);
+        // OrbitControlsの右クリックイベントリスナーを無効化
+        controls.enablePan = false;
+        // イベントハンドラーの設定
+        container.value.addEventListener("contextmenu", onContextMenu);
         // 描画
         animate();
       }
     };
+
     // 描画
-    const animate = () => {
+    const animate = (callback?: () => void) => {
       const frame = () => {
         // カメラの視点変更
         controls.update();
+        // コールバック関数を実行
+        if (callback) callback();
         // 描画
         renderer.render(scene, camera);
         // 画面を更新
@@ -87,6 +127,17 @@ export default defineComponent({
       init();
     });
 
+    // Sphereの作成
+    const createSphere = (): Mesh => {
+      const geometry = new SphereGeometry(
+        SPHERE_RADIUS,
+        SPHERE_SEGMENTS.width,
+        SPHERE_SEGMENTS.height
+      );
+      const material = new MeshLambertMaterial();
+      return new Mesh(geometry, material);
+    };
+
     // ファイル入力時のハンドラー
     const onFileInput = async ({ target }: Event) => {
       if (target instanceof HTMLInputElement && target.files) {
@@ -98,6 +149,79 @@ export default defineComponent({
           scene.add(group);
         }
       }
+    };
+
+    // 右クリック時のハンドラー
+    const onContextMenu = ({ clientX, clientY }: MouseEvent) => {
+      if (container.value instanceof HTMLElement) {
+        // 画面平面内の位置座標を取得
+        const { clientWidth, clientHeight } = container.value;
+        const relativeX = (clientWidth / 2 - clientX) / clientWidth;
+        const relativeY = (clientHeight / 2 - clientY) / clientHeight;
+        // 投げつけるオブジェクトを生成
+        const ball = createSphere();
+        scene.add(ball);
+        // オブジェクトを初期位置に配置
+        setObjectInitialPosition(ball.position, { relativeX, relativeY });
+        // アニメーションを作成して起動
+        moveObject(ball);
+      }
+    };
+
+    // 投げつけるオブジェクトの初期値を設定
+    const setObjectInitialPosition = (
+      position: Vector3,
+      { relativeX, relativeY }: { relativeX: number; relativeY: number }
+    ) => {
+      // カメラが向く方向ベクトルを取得
+      const forward = new Vector3();
+      camera.getWorldDirection(forward);
+      forward.normalize();
+      // カメラからの相対方向ベクトルを取得
+      const { up } = camera; // カメラから見て上方向
+      const left = up.clone().cross(forward); // カメラから見て左方向
+      // 目的の位置へのカメラからの相対位置を計算
+      left.multiplyScalar(relativeX * camera.getFilmWidth()); // 画面左方向
+      const top = up.clone().multiplyScalar(relativeY * camera.getFilmHeight()); // 画面上方向
+      forward // 画面正面方向
+        .multiplyScalar(camera.near); // カメラの視錐台まで前進させる
+      // 位置を設定
+      position.copy(camera.position).add(left).add(top).add(forward);
+    };
+
+    // アニメーションの作成と起動
+    const moveObject = (object: Mesh) => {
+      // アニメーションの始点と終点を計算
+      const startPosition = object.position; // 始点
+      const { x, y, z } = startPosition;
+      const endPosition = new Vector3(); // 終点
+      camera.getWorldDirection(endPosition);
+      endPosition.multiplyScalar(camera.far).add(startPosition); // カメラの向く方向に距離100だけ増加
+      // Keyframeを作成
+      const positionKF = new VectorKeyframeTrack(
+        ".position",
+        [0, ANIMATION_DURATION],
+        [x, y, z, endPosition.x, endPosition.y, endPosition.z]
+      );
+      // Clipを作成
+      const moveObjectClip = new AnimationClip(`move-object-${object.id}`, -1, [
+        positionKF,
+      ]);
+      // Mixerを定義
+      const mixer = new AnimationMixer(object);
+      mixer.addEventListener("finished", () => {
+        // 終了時にオブジェクトを削除
+        scene.remove(object);
+        object.geometry.dispose();
+      });
+      // Actionを定義
+      const action = mixer.clipAction(moveObjectClip);
+      action.setLoop(LoopOnce, 0);
+      // Actionの実行
+      action.play();
+      // アニメーションを更新
+      const clock = new Clock();
+      animate(() => mixer.update(clock.getDelta()));
     };
 
     return {
---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+import { Vector3 } from "three";
+
+export const GRID_SIZE = 50;
+export const BG_COLOR = 0xcccccc;
+export const LIGHT_POSITION = { x: 10, y: 10, z: 0 };
+export const LIGHT_COLOR = 0xffffff;
+export const INITIAL_CAMERA_POSITION = { x: 10, y: 10, z: 0 };
+export const INITIAL_CAMERA_LOOK = new Vector3(0, 0, 0);
+export const AMBIENT_LIGHT_COLOR = 0x404040;
+export const SPHERE_RADIUS = 1;
+export const SPHERE_SEGMENTS = { width: 100, height: 100 };
+export const ANIMATION_DURATION = 10;
